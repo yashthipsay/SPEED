a4815c0d8ec3a61944f3181f52a4b9d91e9ded80
yashthipsay <yashthpsay@gmail.com>
2025-08-02 06:05:25 +0000

    properly storing fields in config files

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # read .env into environment
+
 # --- Celery and WebSocket Configuration ---
 RABBITMQ_URL = 'amqp://guest:guest@localhost:5672/'
 WEBSOCKET_HOST = 'localhost'
 WEBSOCKET_PORT = 8765
 
 # Your AWS credentials
-AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
-AWS_REGION = "AWS_REGION"  # Or your preferred region
-
-# The S3 bucket where order book data will be stored
-AWS_S3_BUCKET_NAME = "AWS_S3_BUCKET_NAME" 
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION', 'AWS_REGION')
+AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
 
 # Capture frequency for the order book data
 DATA_CAPTURE_INTERVAL_SECONDS = 1
